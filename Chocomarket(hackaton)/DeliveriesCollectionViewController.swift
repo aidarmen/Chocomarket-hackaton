ecb7984c271f0944e50df86192c6a772d4923d60
Aidarmen <a87077380001@gmail.com>
2020-05-02 23:49:15 +0600

    added Deliveries  that connects with firebase

--- Chocomarket(hackaton)/DeliveriesCollectionViewController.swift
+++ Chocomarket(hackaton)/DeliveriesCollectionViewController.swift
@@ -10,16 +10,32 @@ import UIKit
 
 
 class DeliveriesCollectionViewController: UICollectionViewController, UICollectionViewDelegateFlowLayout, UINavigationBarDelegate {
+    
+    //MARK: vars
+    var deliveriesArray: [Deliveries] = []
 
+    
+    //MARK: View life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        downloadDeliveriesFromFirebase{
+            (allDeliveries) in
+            
+            print("callback is completed")
+        }
+        
 
         let width = (view.frame.size.width - 50)
         let layout = collectionView.collectionViewLayout as! UICollectionViewFlowLayout
         layout.itemSize = CGSize(width: width, height: width-200)
-
-
-        // Do any additional setup after loading the view.
+    }
+    
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        loadDeliveries()
     }
 
 
@@ -33,47 +49,39 @@ class DeliveriesCollectionViewController: UICollectionViewController, UICollecti
 
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of items
-        return 1
+        return deliveriesArray.count
     }
 
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "deliveryCell", for: indexPath) as! DeliveriesCollectionViewCell
         
-        cell.update(deliveryNumber: "Delivery #\(1)", deliveryDescr: "you should embed each of the view controllers attached to the Tab Bar Controller inside Navigation Control", statusImage: UIImage(systemName: "flag")!, statusName: "Not Taken")
+//        cell.update(deliveryNumber: "Delivery #\(1)", deliveryDescr: "you should embed each of the view controllers attached to the Tab Bar Controller inside Navigation Control", statusImage: UIImage(systemName: "flag")!, statusName: "Not Taken")
+        
+        cell.generateCell(deliveriesArray[indexPath.row])
+        
         cell.layer.cornerRadius = 10
     
         return cell
     }
-
-    // MARK: UICollectionViewDelegate
-
-    /*
-    // Uncomment this method to specify if the specified item should be highlighted during tracking
-    override func collectionView(_ collectionView: UICollectionView, shouldHighlightItemAt indexPath: IndexPath) -> Bool {
-        return true
-    }
-    */
-
-    /*
-    // Uncomment this method to specify if the specified item should be selected
-    override func collectionView(_ collectionView: UICollectionView, shouldSelectItemAt indexPath: IndexPath) -> Bool {
-        return true
-    }
-    */
-
-    /*
-    // Uncomment these methods to specify if an action menu should be displayed for the specified item, and react to actions performed on the item
-    override func collectionView(_ collectionView: UICollectionView, shouldShowMenuForItemAt indexPath: IndexPath) -> Bool {
-        return false
-    }
-
-    override func collectionView(_ collectionView: UICollectionView, canPerformAction action: Selector, forItemAt indexPath: IndexPath, withSender sender: Any?) -> Bool {
-        return false
-    }
-
-    override func collectionView(_ collectionView: UICollectionView, performAction action: Selector, forItemAt indexPath: IndexPath, withSender sender: Any?) {
     
+    //MARK: download delivereis
+    
+    private func loadDeliveries(){
+        downloadDeliveriesFromFirebase{ (allDeliveries) in
+            self.deliveriesArray = allDeliveries
+            
+            self.collectionView.reloadData()
+            
+        }
+        
+        
     }
-    */
+    
+    
+    
+    
+    
+
+   
 
 }
